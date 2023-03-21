8e7e3e15f5c261d8b0d31a633a64efce6b8ae592
a <pswxsj@naver.com>
2023-03-21 23:01:04 +0900

    Fix Conflict

--- quarkdet/model/module/activation.py
+++ quarkdet/model/module/activation.py
@@ -12,10 +12,6 @@ activations = {'ReLU': nn.ReLU,
 def act_layers(name):
     assert name in activations.keys()
     if name == 'LeakyReLU':
-<<<<<<< HEAD
-        return nn.LeakyReLU(negative_slope=0.1)
-=======
         return nn.LeakyReLU(negative_slope=0.1, inplace=True)
->>>>>>> parent of b3ecc5b (Fix inplace = True to False in all ReLU and LeackyReLU)
     else:
         return activations[name](inplace=True)
